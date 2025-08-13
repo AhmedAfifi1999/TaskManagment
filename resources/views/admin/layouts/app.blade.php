b8e79400aa29503346712bdc7823a97517f40807
Ahmed Afifi <ahmedafifi1500@gmail.com>
2025-08-13 12:07:33 +0300

    edit cpanle name

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>{{ config('app.name') }}</title>
+    <title>Task Manager</title>
 
     <meta name="description" content="" />
     @vite('resources/css/app.css')
